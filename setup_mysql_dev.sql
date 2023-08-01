011d05ac36da84367dc155f24e4e73979906cef5
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-01 15:25:17 -0400

    adding commenting and flush privileges

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,16 @@
 -- Setting MySQL server, creates user and applies access
+
+-- creating db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- creating user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- granting view privileges on performance db
 GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
+
+-- grantinng user all privileges on hbnb dev db
 GRANT ALL PRIVILEGES ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
+
+-- applies permission's update without restarting server
+FLUSH PRIVILEGES
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,16 @@
 -- setting the MySQL test server
+
+-- creating db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- creating user
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- granting user all privileges on hbnb test db
 GRANT ALL PRIVILEGES ON 'hbnb_test_db'.* TO 'hbnb_test'@'localhost';
+
+-- granting view privileges on performance db
 GRANT SELECT ON 'performance_schema'.* TO 'hbnb_test'@'localhost';
+
+-- applies permission's update without restarting server
+FLUSH PRIVILEGES;
