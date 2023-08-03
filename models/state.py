aa2601e1b92a60f2f03e0aa078c85e1a5142171d
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-03 17:48:48 -0400

    fixing typos

--- models/state.py
+++ models/state.py
@@ -2,16 +2,19 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='all, delete')
 
     @property
     def cities(self):
         """getter that returns list of cities"""
+        from models import storage
         city_list = []
         all_cities = storage.all(City).values()
 
