d6c85a6853bc7c464f08400e3d3dd1c0e3c91ed5
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-03 15:16:12 -0400

    adding amenity tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,6 +2,34 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
+import unittest
+import pep8
+
+
+class TestAmenityDoc(unittest.TestCase):
+    """ Tests documentation of console file """
+
+    def test_pep8_amenity(self):
+        """Tests that amenity.py conforms to pep8 style guide"""
+        pep8 = pep8.StyleGuide()
+        result = pep8.check_files(['models/amenity.py'])
+        self.assertEqual(result.total_errors, 0, "Got console style errors.")
+
+    def test_amenity_module_docstring(self):
+        """ Test amenity.py module docstrings """
+        self.assertTrue(len(amenity.__doc__) >= 1,
+                        "amenity file needs docstring")
+        self.assertIsNot(amenity.__doc__, None, "amenity file needs docstring")
+
+    def test_pep8_amenity_test(self):
+        """ Test that test_console.py conforms to pep8 style guide"""
+        pep8 = pep8.Styleguide()
+        result = pep8.check_files(['tests/test_models/test_amenity.py'])
+        self.assertEqual(result.total_errors, 0, "Got amenity style errors")
+
+
+if '__name__' == '__main__':
+    unittest.main()
 
 
 class test_Amenity(test_basemodel):
