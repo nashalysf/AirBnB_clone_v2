e3f8ee0924f51e6d66b14b9d759d889f86014056
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-04 15:26:57 -0400

    fixing sql server

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,16 +4,19 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- creating user
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+
+-- setting password
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 
 -- granting user usage to all db & tables
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT USAGE ON *.* TO `hbnb_dev`@`localhost`;
 
 -- granting view privileges on performance db
-GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 
 -- grantinng user all privileges on hbnb dev db
-GRANT ALL PRIVILEGES ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 
 -- applies permission's update without restarting server
 FLUSH PRIVILEGES;
