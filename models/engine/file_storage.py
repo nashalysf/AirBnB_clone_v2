a1a3d30134c4f918e10ec93f72af326f3ce6e075
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-04 11:01:42 -0400

    fixing the delete() typo

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,6 +59,6 @@ class FileStorage:
         """deletes obj from __objects"""
         if obj is not None:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            if key in self.__objects:
-                del self.__objects[key]
+            if key in FileStorage.__objects:
+                del FileStorage.__objects[key]
                 self.save()
