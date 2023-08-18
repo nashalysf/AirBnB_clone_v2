76a66b741c329a5a0c4553592dfe626986e4ee68
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-18 14:36:36 -0400

    fixing typo

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,14 +5,14 @@ Starts a Flask web app
 
 
 from flask import Flask, render_template
-from models import *
+from models import storage
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_list():
     """renders HTML page with the city list"""
-    states = sorted.all("State").values()
+    states = storage.all("State")
     return render_template('8-cities_by_states.html', states=states)
 
 
@@ -21,7 +21,6 @@ def teardown_db(exception):
     """
     closes the storage on teardown
     """
-    from models import storage
     storage.close()
 
 
