9f7a5bc9582a89ae7078c2859a948e243051d047
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-18 14:33:35 -0400

    fixing format

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -6,15 +6,12 @@ Starts a Flask web app
 
 from flask import Flask, render_template
 from models import *
-from models import storage
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_list():
-    """
-    renders HTML page with the city list
-    """
+    """renders HTML page with the city list"""
     states = sorted.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
 
@@ -24,6 +21,7 @@ def teardown_db(exception):
     """
     closes the storage on teardown
     """
+    from models import storage
     storage.close()
 
 
