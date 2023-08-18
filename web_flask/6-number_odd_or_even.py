66d9d52aaa1df8a314dffa46480b548cb14372cb
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-18 14:05:09 -0400

    fixing typos

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""Starts a Flask web app"""
+"""
+Starts a Flask web app
+"""
+
+
 from flask import Flask, render_template
 
 
@@ -9,43 +13,43 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Returns hello at index"""
-    return ("Hello HBNB!")
+    return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Returns hbnb at hbnb"""
-    return ('HBNB')
+    return 'HBNB'
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     """Returns C and value"""
-    return "C " + text.replace('_', " ")
+    return "C " + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
     """Returns C and value"""
-    return "Python " + text.replace('_', " ")
+    return "Python " + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def num(n):
-    """Displays n if is int"""
+    """Displays n if its integer"""
     return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def num_tem(n):
-    """Renders template"""
+    """Renders template if its integer"""
     return render_template('5-number.html', n=n)
 
 
 @app.route('number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_even(n):
-    """Render template odd or even if int"""
+    """Render template odd or even if integer"""
     if n % 2 == 0:
         even = 'even'
     else:
